a6173b9a73df8f55e9abf145e00851b0c737f995
MicheleLafronza <michele.lafronza91@gmail.com>
2024-07-18 16:40:17 +0200

    feat: accordion component into StartHere page complete

--- src/pages/startherecomponents/Accordion.vue
+++ src/pages/startherecomponents/Accordion.vue
@@ -8,16 +8,19 @@ export default {
           question: "How to register online?",
           answer:
             "Enrolling in an online class is easy. At MaxCoach, we serve several categories of online learners. Select the student category that you identify with to get started.",
+          open: false,
         },
         {
           question: "How do I customize my account?",
           answer:
             "In order to customize your accounts information, just log in with the provided passcode and change the info. ",
+          open: false,
         },
         {
           question: "How do you process my information?",
           answer:
             "Your data will be kept private and secure on our database. No illegal use without prior notice.",
+          open: false,
         },
       ],
       accordionCardsRight: [
@@ -25,19 +28,32 @@ export default {
           question: "How do I contact support?",
           answer:
             "You can contact our help center or any contact method (phone call, email or appear at our office) to get help.",
+          open: false,
         },
         {
           question: "How to cancel my membership?",
           answer:
             "You just need to submit a membership termination request, well proceed within 24 hours.",
+          open: false,
         },
         {
-          question: "How can i get a discount?",
+          question: "How can I get a discount?",
           answer:
             "Learners can get a discount by coupon or voucher. If you have one, fill in the code box before making any payment.",
+          open: false,
         },
       ],
     };
+  },
+  methods: {
+    openCardLeft(index) {
+      this.accordionCardsLeft[index].open =
+        !this.accordionCardsLeft[index].open;
+    },
+    openCardRight(index) {
+      this.accordionCardsRight[index].open =
+        !this.accordionCardsRight[index].open;
+    },
   },
 };
 </script>
@@ -52,20 +68,42 @@ export default {
       <div
         v-for="(leftCard, index) in accordionCardsLeft"
         :key="index"
-        class="card"
+        class="out-accordion"
+        @click="openCardLeft(index)"
       >
-        <div class="question">{{ leftCard.question }}</div>
-        <span><i class="fa-solid fa-plus"></i></span>
+        <div class="card">
+          <div class="question">{{ leftCard.question }}</div>
+          <span v-if="leftCard.open === false"
+            ><i class="fa-solid fa-circle-plus"></i
+          ></span>
+          <span v-else><i class="fa-solid fa-circle-minus"></i></span>
+        </div>
+        <Transition>
+          <div class="answer" v-show="leftCard.open">
+            {{ leftCard.answer }}
+          </div>
+        </Transition>
       </div>
     </div>
     <div class="inner-accordion right">
       <div
         v-for="(rightCard, index) in accordionCardsRight"
         :key="index"
-        class="card"
+        class="out-accordion"
+        @click="openCardRight(index)"
       >
-        <div class="question">{{ rightCard.question }}</div>
-        <span><i class="fa-solid fa-plus"></i></span>
+        <div class="card">
+          <div class="question">{{ rightCard.question }}</div>
+          <span v-if="rightCard.open === false"
+            ><i class="fa-solid fa-circle-plus"></i
+          ></span>
+          <span v-else><i class="fa-solid fa-circle-minus"></i></span>
+        </div>
+        <Transition>
+          <div class="answer" v-show="rightCard.open">
+            {{ rightCard.answer }}
+          </div>
+        </Transition>
       </div>
     </div>
   </div>
@@ -93,6 +131,16 @@ export default {
   }
 }
 
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
 .accordion {
   width: 80%;
   margin: 0 auto;
@@ -103,13 +151,41 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    //   debug
-    border: 1px solid black;
+    margin-right: 20px;
 
-    .card {
-      display: flex;
-      justify-content: space-between;
-      padding: 10px 8px;
+    .out-accordion {
+      margin-bottom: 20px;
+
+      .card {
+        display: flex;
+        justify-content: space-between;
+        padding: 20px 16px;
+        color: $main-green;
+        font-weight: 600;
+        border: 1px solid rgba($color: #000000, $alpha: 0.25);
+        border-radius: 10px;
+        // margin-bottom: 30px;
+        transition: all 0.3 ease;
+
+        &:hover {
+          background-color: $main-green;
+          color: white;
+          cursor: pointer;
+        }
+
+        .fa-circle-plus,
+        .fa-circle-minus {
+          font-size: 1.5rem;
+        }
+      }
+      .answer {
+        padding: 20px;
+        font-size: 14px;
+        font-weight: 300;
+        color: $main-gray;
+        box-shadow: 10px 10px 20px grey;
+        border-radius: 0px 0px 10px 10px;
+      }
     }
   }
 }
